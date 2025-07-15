ae8d649fc80b3b2d3c522cb3f14d8235facaad0d
Copeland Carter <smartycope@gmail.com>
2025-07-15 10:23:08 -0500

    feat: add direct input tab for editing answers

--- src/quiz.py
+++ src/quiz.py
@@ -6,41 +6,31 @@ import pandas as pd
 if "idx" not in ss:
     ss.idx = 0
 
+
 st.title("Answers")
 if ss.decision is None:
     st.warning("Please add a decision first")
     st.stop()
 
-l, m, r = st.columns(3)
-if l.button("Go to the beginning"):
-    ss.idx = 0
-if m.button('Delete all'):
-    ss.decision.clear_all_answers()
-    ss.idx = 0
-
-columnar = not r.checkbox('Left to Right', value=True)
-
-st.divider()
 
 
 def index():
     if ss.idx >= len(ss.decision.factors['names']) * len(ss.decision.options):
         ss.idx = 0
-    if columnar:
+    if not ss.anticolumnar:
         return ss.idx % len(ss.decision.options), ss.idx // len(ss.decision.options)
     else:
         return ss.idx // (len(ss.decision.options)+1), ss.idx % (len(ss.decision.options)+1)
 
-
-def show_answers():
-    # st.dataframe(np.vectorize(lambda x:f'{x[0]}-{x[1]}' if x[0] != x[1] else str(x[0]), signature='(2)->()')(ss.decision.answers))
-        # return np.fromiter((f(xi) for xi in x), x.dtype)
-    # st.dataframe(np.fromiter(((f'{xi[0]}-{xi[1]}' if xi[0] != xi[1] else str(xi[0])) for xi in ss.decision.answers), ss.decision.answers.dtype).reshape(ss.decision.answers.shape))
+def formatted_answers():
     def formatter(x):
         if np.isnan(x[0]) or np.isnan(x[1]):
             return None
         return f'{x[0]:.0f}-{x[1]:.0f}' if x[0] != x[1] else f'{x[0]:.0f}'
-    df = np.array(list(map(formatter, ss.decision.answers.reshape(-1, 2)))).reshape(ss.decision.answers.shape[:2])
+    return np.array(list(map(formatter, ss.decision.answers.reshape(-1, 2)))).reshape(ss.decision.answers.shape[:2])
+
+def show_answers_static():
+    df = formatted_answers()
     # highlight the current index
     target_index, target_column = index()
 
@@ -55,49 +45,87 @@ def show_answers():
 
     st.dataframe(pd.DataFrame(df, columns=ss.decision.factors['names'], index=ss.decision.options).style.pipe(style))
 
+def quiz():
+    l, m, r = st.columns(3)
+    if l.button("Go to the beginning"):
+        ss.idx = 0
+    if m.button('Delete all'):
+        ss.decision.clear_all_answers()
+        ss.idx = 0
 
+    r.checkbox('Left to Right', value=True, key='anticolumnar')
 
-option = ss.decision.options[index()[0]]
-factor = ss.decision.factors['names'][index()[1]]
-st.title(f"On a scale of {ss.decision.factors['units'][index()[1]]}, how much `{factor}` does `{option}` have?")
+    st.divider()
 
-scale_min = float(ss.decision.factors['mins'][index()[1]]) if ss.decision.factors['mins'][index()[1]] is not None else None
-scale_max = float(ss.decision.factors['maxs'][index()[1]]) if ss.decision.factors['maxs'][index()[1]] is not None else None
-current = ss.decision.get_answer(option, factor)# or ss.decision.factors['optimals'][index()[1]]
-if current is not None and not np.isnan(current).any():
-    cur_min, cur_max = current
-else:
-    cur_min = cur_max = ss.decision.factors['optimals'][index()[1]]
+    option = ss.decision.options[index()[0]]
+    factor = ss.decision.factors['names'][index()[1]]
+    st.title(f"On a scale of {ss.decision.factors['units'][index()[1]]}, how much `{factor}` does `{option}` have?")
 
-unsure = st.checkbox("I'm not sure", value=cur_min != cur_max)
-
-type = st.number_input if scale_min is None or scale_max is None else st.slider
-if unsure:
-    min_resp = type(label='Minimum ' + factor, min_value=scale_min, max_value=scale_max, value=cur_min)
-    max_resp = type(label='Maximum ' + factor, min_value=scale_min, max_value=scale_max, value=cur_max)
-else:
-    resp = type(label=factor, min_value=scale_min, max_value=scale_max, value=float(cur_min))
-
-
-
-l, m, r = st.columns(3)
-if m.button("Submit"):
-    if unsure:
-        ss.decision.set_answer(option, factor, [min_resp, max_resp])
+    scale_min = float(ss.decision.factors['mins'][index()[1]]) if ss.decision.factors['mins'][index()[1]] is not None else None
+    scale_max = float(ss.decision.factors['maxs'][index()[1]]) if ss.decision.factors['maxs'][index()[1]] is not None else None
+    current = ss.decision.get_answer(option, factor)# or ss.decision.factors['optimals'][index()[1]]
+    if current is not None and not np.isnan(current).any():
+        cur_min, cur_max = current
     else:
-        ss.decision.set_answer(option, factor, resp)
-    ss.idx += 1
-    st.rerun()
+        cur_min = cur_max = ss.decision.factors['optimals'][index()[1]]
 
-if r.button("Skip"):
-    ss.idx += 1
-    st.rerun()
+    unsure = st.checkbox("I'm not sure", value=cur_min != cur_max)
 
-if l.button("Back"):
-    ss.idx -= 1
-    st.rerun()
+    type = st.number_input if scale_min is None or scale_max is None else st.slider
+    if unsure:
+        min_resp = type(label='Minimum ' + factor, min_value=scale_min, max_value=scale_max, value=cur_min)
+        max_resp = type(label='Maximum ' + factor, min_value=scale_min, max_value=scale_max, value=cur_max)
+    else:
+        resp = type(label=factor, min_value=scale_min, max_value=scale_max, value=float(cur_min))
 
-st.divider()
-show_answers()
+    l, m, r = st.columns(3)
+    if m.button("Submit"):
+        if unsure:
+            ss.decision.set_answer(option, factor, [min_resp, max_resp])
+        else:
+            ss.decision.set_answer(option, factor, resp)
+        ss.idx += 1
+        st.rerun()
+
+    if r.button("Skip"):
+        ss.idx += 1
+        st.rerun()
+
+    if l.button("Back"):
+        ss.idx -= 1
+        st.rerun()
+
+def direct_input():
+    df = st.data_editor(pd.DataFrame(formatted_answers(), columns=ss.decision.factors['names'], index=ss.decision.options))
+    # Get the index which was changed
+    changed = df[df != formatted_answers()].stack().reset_index()
+    st.write(changed)
+    # st.write(changed['level_0'])
+    # st.write(changed['level_1'])
+    # st.write(changed[0])
+    # make sure the new value is within the min and max
+    for _, j in changed.iterrows():
+        try:
+            ss.decision.set_answer(j['level_0'], j['level_1'], j[0])
+        except ValueError as e:
+            # factor_min = ss.decision.factors['mins'][ss.decision.factors['names'].index(j['level_1'])]
+            # factor_max = ss.decision.factors['maxs'][ss.decision.factors['names'].index(j['level_1'])]
+            # st.warning(f"Invalid answer or answer out of bounds: \"{j[0]}\" (min: {factor_min}, max: {factor_max})")
+            st.error(e)
+            print(e)
+            st.exception(e)
+            # st.error(str(e))
+
+            # st.rerun()
+    st.write(df)
+    st.write(formatted_answers())
+
+a, b = st.tabs(['Quiz', 'Direct Input'])
+with a:
+    quiz()
+    show_answers_static()
+with b:
+    direct_input()
+# st.divider()
 
 st.sidebar.write(ss.texts['answers']['explanation'])
